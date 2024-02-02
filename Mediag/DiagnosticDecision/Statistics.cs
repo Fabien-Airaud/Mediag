ebd84948a221938f750b9380846346a2bf89987e
Fabien-Airaud <airaudfabien@gmail.com>
2024-02-02 17:42:48 -0500

    Correct initialisation

--- Mediag/DiagnosticDecision/Statistics.cs
+++ Mediag/DiagnosticDecision/Statistics.cs
@@ -4,10 +4,10 @@ namespace Mediag.DiagnosticDecision
 {
     class Statistics
     {
-        public List<object[]> Set { get; set; }
+        public List<object[]> Set { get; set; } = new List<object[]>();
 
 
-        public Statistics() : this(new List<object[]>()) { }
+        public Statistics() { }
         public Statistics(List<object[]> set) { Set = set; }
     }
 }
