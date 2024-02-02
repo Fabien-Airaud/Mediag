861d010e7f4ce4e30c05d4d2c5cc3a5f0970b44b
Fabien-Airaud <airaudfabien@gmail.com>
2024-02-02 18:24:24 -0500

    Correct probability function when check wrong

--- Mediag/DiagnosticDecision/Statistics.cs
+++ Mediag/DiagnosticDecision/Statistics.cs
@@ -18,19 +18,19 @@ namespace Mediag.DiagnosticDecision
             return set.Count == 0 || factor < 0 || factor >= set[0].Length;
         }
 
-        public double ProbabilityFactor(int factor, object value)
+        private double ProbabilityFactor(int factor, object value)
         {
-            if (Check(Set, factor)) return 0;
+            if (Check(Set, factor)) return -1;
 
             return (double)Set.Count(row => row[factor].Equals(value)) / Set.Count;
         }
 
-        public double ProbabilityResultKnowingFactor(int result, object valueResult, int factor, object valueFactor)
+        private double ProbabilityResultKnowingFactor(int result, object valueResult, int factor, object valueFactor)
         {
-            if (Check(Set, factor)) return 0;
+            if (Check(Set, factor)) return -1;
 
             List<object[]> setFactor = Set.FindAll(row => row[factor].Equals(valueFactor));
-            if (Check(setFactor, result)) return 0;
+            if (Check(setFactor, result)) return -1;
 
             return (double)setFactor.Count(row => row[result].Equals(valueResult)) / setFactor.Count;
         }
