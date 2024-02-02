d0d14c4837970511db90fefa0da1117e8ff3d551
Fabien-Airaud <airaudfabien@gmail.com>
2024-02-02 15:17:32 -0500

    Can change database folder for csv and add default one

--- Mediag/Database/CsvManager.cs
+++ Mediag/Database/CsvManager.cs
@@ -1,6 +1,5 @@
 ﻿using CsvHelper;
 using Mediag.Hospital;
-using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
@@ -9,27 +8,34 @@ namespace Mediag.Database
 {
     class CsvManager
     {
-        private static readonly string SamplesFilename = "database/samples.csv";
+        private static readonly string DefaultDatabase = "database";
+
+        private static readonly string SamplesFilename = "samples.csv";
 
 
-        public static List<Diagnosis> GetTrainDiagnosis()
+        public static List<Diagnosis> GetTrainDiagnosis() { return GetTrainDiagnosis(DefaultDatabase); }
+        public static List<Diagnosis> GetTrainDiagnosis(string databaseFolder)
         {
             // Change to get train set
-            return GetSamplesDiagnosis();
+            return GetSamplesDiagnosis(databaseFolder);
         }
 
-        public static List<Diagnosis> GetTestDiagnosis()
+        public static List<Diagnosis> GetTestDiagnosis() { return GetTestDiagnosis(DefaultDatabase); }
+        public static List<Diagnosis> GetTestDiagnosis(string databaseFolder)
         {
             // Change to get test set
-            return GetSamplesDiagnosis();
+            return GetSamplesDiagnosis(databaseFolder);
         }
 
-        public static List<Diagnosis> GetSamplesDiagnosis()
+        public static List<Diagnosis> GetSamplesDiagnosis() { return GetSamplesDiagnosis(DefaultDatabase); }
+        public static List<Diagnosis> GetSamplesDiagnosis(string databaseFolder)
         {
-            if (!File.Exists(SamplesFilename)) { return null; }
+            string filename = Path.Combine(databaseFolder, SamplesFilename);
+            if (!File.Exists(filename)) { return null; }
+
             List<Diagnosis> diagnosisList = new List<Diagnosis>();
 
-            using (var reader = new StreamReader(SamplesFilename))
+            using (var reader = new StreamReader(filename))
             using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
             {
                 csv.Context.RegisterClassMap<DiagnosisMap>();
