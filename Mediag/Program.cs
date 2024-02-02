2fbf5ab6ac1d816c7777a1f8170fc4fd5f80d2c6
Fabien-Airaud <airaudfabien@gmail.com>
2024-02-02 11:20:57 -0500

    Try to use CSV manager with samples

--- Mediag/Program.cs
+++ Mediag/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace Mediag
+﻿using Mediag.Database;
+using Mediag.Hospital;
+using System;
+using System.Collections.Generic;
+
+namespace Mediag
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            List<Diagnosis> data = CsvManager.GetDiagnoses("database/samples.csv");
+            Console.WriteLine(data == null ? "File doesn't exist" : data.ToString());
         }
     }
 }
