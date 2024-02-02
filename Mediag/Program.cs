ce5f5ef0352b6ce132fdcaaeaa6103184a350fac
Fabien-Airaud <airaudfabien@gmail.com>
2024-02-02 12:09:25 -0500

    Test Values fonctions of IForDecisionTree with Diagnosis

--- Mediag/Program.cs
+++ Mediag/Program.cs
@@ -2,6 +2,7 @@
 using Mediag.Hospital;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace Mediag
 {
@@ -11,6 +12,19 @@ namespace Mediag
         {
             List<Diagnosis> data = CsvManager.GetDiagnoses("database/samples.csv");
             Console.WriteLine(data == null ? "File doesn't exist" : data.ToString());
+
+            foreach (object value in data[0].Values())
+            {
+                Console.WriteLine(value.GetType().Name + " = " + value.ToString());
+            }
+
+            //PropertyInfo[] propertyInfos = data[0].GetType().GetProperties();
+            //foreach (PropertyInfo propertyInfo in propertyInfos)
+            //{
+            //    Console.WriteLine(propertyInfo.Name);
+            //}
+            //Console.WriteLine(propertyInfos[0].GetValue(data[0]));
+            //Console.WriteLine(data[0].GetType().GetProperty("Id").GetValue(data[0]));
         }
     }
 }
