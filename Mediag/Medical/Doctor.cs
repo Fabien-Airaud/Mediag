d9f90d71c3f591e6d6a5b6389a6dfc3e2db754df
Fabien-Airaud <airaudfabien@gmail.com>
2024-02-11 15:59:49 -0500

    Create class Doctor extending Person

---
+++ Mediag/Medical/Doctor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Mediag.Medical
+{
+    class Doctor : Person
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public bool Active { get; set; } = true;
+
+
+        public Doctor() : base() { }
+
+        public Doctor(long id, string lastName, string firstName, DateTime birthdate, string phoneNumber, string email, string address)
+            : base(id, lastName, firstName, birthdate, phoneNumber, email, address)
+        {
+            Username = firstName + lastName;
+            Password = lastName.ToUpper()[0] + firstName.ToLower()[0] + birthdate.ToString("yyyy-MM-dd");
+        }
+    }
+}
