1c67a3780fd060b5749f4d55728b94521824330e
Fabien-Airaud <airaudfabien@gmail.com>
2024-02-11 15:35:41 -0500

    Create abstract class Personn

---
+++ Mediag/Medical/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Mediag.Medical
+{
+    abstract class Person
+    {
+        public long Id { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public DateTime Birthdate { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+
+
+        public override string ToString()
+        {
+            return GetType().Name + " " + FirstName + " " + LastName;
+        }
+    }
+}
