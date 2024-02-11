40ac01709f0a4fd4837d642eb7ea6dfdf47b667e
Fabien-Airaud <airaudfabien@gmail.com>
2024-02-11 15:25:17 -0500

    Create class Hospital in Medical folder

---
+++ Mediag/Medical/Hospital.cs
@@ -0,0 +1,21 @@
+﻿namespace Mediag.Medical
+{
+    class Hospital
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+
+        public Hospital() { }
+
+        public Hospital(long id, string name, string address)
+        {
+            Id = id;
+            Name = name;
+            Address = address;
+        }
+    }
+}
