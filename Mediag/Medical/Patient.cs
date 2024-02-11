b13461d07f72828768feb5397d1269981678e798
Fabien-Airaud <airaudfabien@gmail.com>
2024-02-11 15:41:54 -0500

    Create class Patient extending Person

---
+++ Mediag/Medical/Patient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mediag.Medical
+{
+    class Patient : Person
+    {
+        public Patient() : base() { }
+
+        public Patient(long id, string lastName, string firstName, DateTime birthdate, string phoneNumber, string email, string address)
+            : base(id, lastName, firstName, birthdate, phoneNumber, email, address) { }
+    }
+}
