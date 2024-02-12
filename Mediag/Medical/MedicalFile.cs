8cf65bb9d1cd35a35d63e622d1d27221f221873a
Fabien-Airaud <airaudfabien@gmail.com>
2024-02-11 20:50:50 -0500

    Create MedicalFile

---
+++ Mediag/Medical/MedicalFile.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace Mediag.Medical
+{
+    class MedicalFile
+    {
+        private static long lastId = 0;
+
+        public long Id { get; set; } = ++lastId;
+
+        public DateTime StartDate { get; set; } = DateTime.Now;
+
+        public DateTime LastUpdate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        /// <summary>
+        /// Must be initialized
+        /// </summary>
+        public Patient Patient { get; set; }
+
+        /// <summary>
+        /// Must be initialized
+        /// </summary>
+        public Hospital Hospital { get; set; }
+
+
+        public MedicalFile() { }
+
+        public MedicalFile(Patient patient, Hospital hospital)
+        {
+            Patient = patient;
+            Hospital = hospital;
+        }
+
+        public MedicalFile(DateTime startDate, Patient patient, Hospital hospital)
+        {
+            StartDate = startDate;
+            Patient = patient;
+            Hospital = hospital;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Medical file {Id}: {Patient.FirstName} {Patient.LastName}";
+        }
+    }
+}
