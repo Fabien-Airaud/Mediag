21a2b16daa236eb25f82145f3ede066b8c4249f7
Fabien-Airaud <airaudfabien@gmail.com>
2024-02-16 18:02:41 -0500

    Create BreastCancerData class

---
+++ Mediag/Medical/BreastCancerData.cs
@@ -0,0 +1,37 @@
+﻿namespace Mediag.Medical
+{
+    class BreastCancerData
+    {
+        private static long lastId = 0;
+
+        public long Id { get; protected set; } = ++lastId;
+
+        public double RadiusWorst { get; set; }
+
+        public double AreaWorst { get; set; }
+
+        public double PerimeterWorst { get; set; }
+
+        public double ConcavePointsWorst { get; set; }
+
+        public double ConcavePointsMean { get; set; }
+
+        public double PerimeterMean { get; set; }
+
+        public bool Result { get; set; }
+
+
+        public BreastCancerData() { }
+
+        public BreastCancerData(double radiusWorst, double areaWorst, double perimeterWorst, double concavePointsWorst, double concavePointsMean, double perimeterMean, bool result)
+        {
+            RadiusWorst = radiusWorst;
+            AreaWorst = areaWorst;
+            PerimeterWorst = perimeterWorst;
+            ConcavePointsWorst = concavePointsWorst;
+            ConcavePointsMean = concavePointsMean;
+            PerimeterMean = perimeterMean;
+            Result = result;
+        }
+    }
+}
