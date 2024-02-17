6ed59c431582a2b5c9aaca3c3e5557b87f9c9f09
Fabien-Airaud <airaudfabien@gmail.com>
2024-02-16 19:09:26 -0500

    Create DataManager to get data from CSV

---
+++ Mediag/DatabaseCSV/DataManager.cs
@@ -0,0 +1,39 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using Mediag.Medical;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace Mediag.DatabaseCSV
+{
+    class DataManager<T, K> where T : IMedicalData where K : ClassMap<T>
+    {
+        public string DatabaseFolder { get; set; }
+
+        public string SamplesFilename { get; set; } = "samples.csv";
+
+
+        public DataManager(string databaseFolder)
+        {
+            DatabaseFolder = databaseFolder;
+        }
+
+
+        public List<T> GetSamplesData()
+        {
+            string filename = Path.Combine(DatabaseFolder, SamplesFilename);
+            if (!File.Exists(filename)) { return null; }
+
+            List<T> dataList = null;
+            using (var reader = new StreamReader(filename))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                csv.Context.RegisterClassMap<K>();
+                var records = csv.GetRecords<T>();
+                dataList = new List<T>(records);
+            }
+            return dataList;
+        }
+    }
+}
