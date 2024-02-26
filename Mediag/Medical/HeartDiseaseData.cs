8f8ee463d63107c74fa1ec6723c355948df27546
Fabien-Airaud <airaudfabien@gmail.com>
2024-02-26 16:07:43 -0500

    Create HeartDiseaseData with just result

---
+++ Mediag/Medical/HeartDiseaseData.cs
@@ -0,0 +1,32 @@
+﻿namespace Mediag.Medical
+{
+    class HeartDiseaseData : IMedicalData
+    {
+        private static long lastId = 0;
+
+        public long Id { get; protected set; } = ++lastId;
+
+        public bool Result { get; set; }
+
+
+        public HeartDiseaseData() { }
+
+        public HeartDiseaseData(bool result)
+        {
+            Result = result;
+        }
+
+
+        public IllnessTypes TargettedIllness() { return IllnessTypes.BreastCancer; }
+
+        public string[] Values()
+        {
+            return [Result.ToString()];
+        }
+
+        public string[] Labels()
+        {
+            return ["Result"];
+        }
+    }
+}
