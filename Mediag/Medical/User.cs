f689608a6acf023b5b277505e21abd34cc8bd058
Fabien-Airaud <airaudfabien@gmail.com>
2024-03-12 18:05:39 -0400

    Create User extending Person

---
+++ Mediag/Medical/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Mediag.Medical
+{
+    abstract class User : Person
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public bool Active { get; set; } = true;
+
+
+        public User(string lastName, string firstName, DateOnly birthdate, string phoneNumber, string email, string address)
+            : base(lastName, firstName, birthdate, phoneNumber, email, address)
+        {
+            Username = firstName + lastName;
+            Password = lastName.ToUpper()[0] + firstName.ToLower()[0] + birthdate.ToString("yyyy-MM-dd");
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name} {Id}: {FirstName} {LastName} ({Username})";
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is User &&
+                   base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(base.GetHashCode(), Username);
+        }
+    }
+}
