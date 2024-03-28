9462b09026590d94023d359eed0d8f8f1d97f460
Fabien-Airaud <airaudfabien@gmail.com>
2024-03-27 20:02:54 -0400

    Rename Mediag console project

--- MediagConsole/Medical/Admin.cs
+++ MediagConsole/Medical/Admin.cs
@@ -10,6 +10,7 @@ namespace Mediag.Medical
         public Admin(string lastName, string firstName, DateOnly birthdate, string phoneNumber, string email, string address)
             : base(lastName, firstName, birthdate, phoneNumber, email, address) { Id = ++lastId; }
 
+
         public override bool Equals(object obj)
         {
             return obj is Admin &&
