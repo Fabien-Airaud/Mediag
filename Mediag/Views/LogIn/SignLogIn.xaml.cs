5b08ed9dafbae095c343ca47e0b7f21356e45a2c
Fabien-Airaud <airaudfabien@gmail.com>
2024-03-30 16:24:26 -0400

    Move SignLogIn page to LogIn folder and add tabControl

---
+++ Mediag/Views/LogIn/SignLogIn.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Mediag.Views.LogIn
+{
+    /// <summary>
+    /// Interaction logic for SignLogIn.xaml
+    /// </summary>
+    public partial class SignLogIn : Window
+    {
+        public SignLogIn()
+        {
+            InitializeComponent();
+        }
+    }
+}
