2ba9208e8572ccc0f5f666cf42a0e06d4a4a5792
Fabien-Airaud <airaudfabien@gmail.com>
2024-03-31 16:06:58 -0400

    Create MedicalFiles pages (datagrid) with action icons

---
+++ Mediag/Views/Principal/MedicalFiles.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Mediag.Views.Principal
+{
+    /// <summary>
+    /// Interaction logic for MedicalFiles.xaml
+    /// </summary>
+    public partial class MedicalFiles : Page
+    {
+        public MedicalFiles()
+        {
+            InitializeComponent();
+        }
+    }
+}
