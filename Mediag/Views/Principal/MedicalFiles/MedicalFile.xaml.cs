8f5f45d4c2791c03b42fe30410e2d077a23e81a6
Fabien-Airaud <airaudfabien@gmail.com>
2024-03-31 21:30:13 -0400

    Create MedicalFile empty window

---
+++ Mediag/Views/Principal/MedicalFiles/MedicalFile.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Mediag.Views.Principal.MedicalFiles
+{
+    /// <summary>
+    /// Interaction logic for MedicalFile.xaml
+    /// </summary>
+    public partial class MedicalFile : Window
+    {
+        public MedicalFile()
+        {
+            InitializeComponent();
+        }
+    }
+}
