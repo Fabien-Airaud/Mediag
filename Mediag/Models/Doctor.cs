6ad4ccf63c6fdf29597297a1d8fbfdd48bef36e5
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-04 18:45:48 -0400

    Add specialism to doctors

--- Mediag/Models/Doctor.cs
+++ Mediag/Models/Doctor.cs
@@ -2,5 +2,18 @@
 {
     class Doctor : User
     {
+        private string _specialism = "";
+        public string Specialism
+        {
+            get { return _specialism; }
+            set
+            {
+                if (_specialism != value)
+                {
+                    _specialism = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
     }
 }
