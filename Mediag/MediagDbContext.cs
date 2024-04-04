8b4b2329c9608ae80d253c4b726c2b89e43587a5
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-04 18:20:14 -0400

    Add doctors to db

--- Mediag/MediagDbContext.cs
+++ Mediag/MediagDbContext.cs
@@ -4,6 +4,8 @@ namespace Mediag
 {
     class MediagDbContext : DbContext
     {
+        public DbSet<Models.Doctor> Doctors { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
@@ -11,12 +13,12 @@ namespace Mediag
             optionsBuilder.UseSqlServer($"{connectionString};Database={databaseName};");
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            //modelBuilder.Entity<Person>().HasData(
-            //    new Person { Id = 1, Name = "John Doe", Age = 30 },
-            //    new Person { Id = 2, Name = "Jane Doe", Age = 25 }
-            //);
-        }
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Person>().HasData(
+        //        new Person { Id = 1, Name = "John Doe", Age = 30 },
+        //        new Person { Id = 2, Name = "Jane Doe", Age = 25 }
+        //    );
+        //}
     }
 }
