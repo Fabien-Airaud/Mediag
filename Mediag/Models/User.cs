5dcc199df578359c4facfe2faedd4e4b1f411f3f
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-04 18:36:46 -0400

    Use onPropertyChanger on each properties of User

--- Mediag/Models/User.cs
+++ Mediag/Models/User.cs
@@ -4,8 +4,32 @@
         string username, string password)
         : Person(lastName, firstName, birthdate, phoneNumber, email, city)
     {
-        public string Username { get; set; } = username;
+        private string _username = username;
+        public string Username
+        {
+            get { return _username; }
+            set
+            {
+                if (_username != value)
+                {
+                    _username = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
-        public string Password { get; set; } = password;
+        private string _password = password;
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                if (_password != value)
+                {
+                    _password = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
     }
 }
