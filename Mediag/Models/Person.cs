fa4662c4984a3685c6350eef5862589f9851f8eb
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-04 18:08:13 -0400

    Create Person model

---
+++ Mediag/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace Mediag.Medical
+{
+    abstract class Person(string lastName, string firstName, DateOnly birthdate, string phoneNumber, string email, string city)
+    {
+        public string LastName { get; set; } = lastName;
+
+        public string FirstName { get; set; } = firstName;
+
+        public DateOnly Birthdate { get; set; } = birthdate;
+
+        public string PhoneNumber { get; set; } = phoneNumber;
+
+        public string Email { get; set; } = email;
+
+        public string City { get; set; } = city;
+    }
+}
