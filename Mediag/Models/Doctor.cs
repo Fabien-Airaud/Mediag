95927c7c16149e96657b11c8badad7555be09d4d
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-04 18:08:25 -0400

    Create Doctor model

---
+++ Mediag/Models/Doctor.cs
@@ -0,0 +1,9 @@
+﻿namespace Mediag.Medical
+{
+    class Doctor(string lastName, string firstName, DateOnly birthdate, string phoneNumber, string email, string city,
+        string username, string password)
+        : User(lastName, firstName, birthdate, phoneNumber, email, city, username, password)
+    {
+        public long DoctorId { get; set; }
+    }
+}
