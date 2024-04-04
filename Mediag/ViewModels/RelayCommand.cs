4f9a4b7f4d638cb65c2dd689f8ce4e4128166553
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-04 19:56:42 -0400

    Create RelayCommand

---
+++ Mediag/ViewModels/RelayCommand.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Input;
+
+namespace Mediag.ViewModels
+{
+    public class RelayCommand(Predicate<object?> canExecute, Action<object?> execute) : ICommand
+    {
+        private readonly Predicate<object?> _canExecute = canExecute;
+        private readonly Action<object?> _execute = execute;
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object? parameter) { return _canExecute(parameter); }
+
+        public void Execute(object? parameter) { _execute(parameter); }
+    }
+}
