dec35940248db3fced7bece334a37d895bd3cc14
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-04 18:08:19 -0400

    Create User model

---
+++ Mediag/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Mediag.Medical
+{
+    abstract class User(string lastName, string firstName, DateOnly birthdate, string phoneNumber, string email, string city,
+        string username, string password)
+        : Person(lastName, firstName, birthdate, phoneNumber, email, city)
+    {
+        public string Username { get; set; } = username;
+
+        public string Password { get; set; } = password;
+    }
+}
