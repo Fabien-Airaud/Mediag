cb2eaae6680d44a6ae6d9d4d9bf9085c4cfc2457
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-04 18:31:43 -0400

    Extend INotifyPropertyChanged

--- Mediag/Models/Person.cs
+++ Mediag/Models/Person.cs
@@ -1,6 +1,10 @@
-﻿namespace Mediag.Models
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Mediag.Models
 {
     abstract class Person(string lastName, string firstName, DateOnly birthdate, string phoneNumber, string email, string city)
+        : INotifyPropertyChanged
     {
         public long Id { get; set; }
 
@@ -15,5 +19,13 @@
         public string Email { get; set; } = email;
 
         public string City { get; set; } = city;
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
