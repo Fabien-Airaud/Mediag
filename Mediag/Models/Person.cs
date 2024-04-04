1acdc76283d71d03b0649f36ef6a90f1bab5481f
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-04 18:34:32 -0400

    Add onPropertyChanged for each properties

--- Mediag/Models/Person.cs
+++ Mediag/Models/Person.cs
@@ -8,17 +8,89 @@ namespace Mediag.Models
     {
         public long Id { get; set; }
 
-        public string LastName { get; set; } = lastName;
+        private string _lastName = lastName;
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                if (_lastName != value)
+                {
+                    _lastName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
-        public string FirstName { get; set; } = firstName;
+        private string _firstName = firstName;
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                if (_firstName != value)
+                {
+                    _firstName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
-        public DateOnly Birthdate { get; set; } = birthdate;
+        private DateOnly _birthdate = birthdate;
+        public DateOnly Birthdate
+        {
+            get { return _birthdate; }
+            set
+            {
+                if (_birthdate != value)
+                {
+                    _birthdate = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
-        public string PhoneNumber { get; set; } = phoneNumber;
+        private string _phoneNumber = phoneNumber;
+        public string PhoneNumber
+        {
+            get { return _phoneNumber; }
+            set
+            {
+                if (_phoneNumber != value)
+                {
+                    _phoneNumber = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
-        public string Email { get; set; } = email;
+        private string _email = email;
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                if (_email != value)
+                {
+                    _email = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
-        public string City { get; set; } = city;
+        private string _city = city;
+        public string City
+        {
+            get { return _city; }
+            set
+            {
+                if (_city != value)
+                {
+                    _city = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
 
         public event PropertyChangedEventHandler? PropertyChanged;
