1abc028699ea26d254827c2ec91fc4050394a920
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-04 21:37:56 -0400

    Rename ViewModel for signLogIn and add start of command

---
+++ Mediag/ViewModels/SignLogInViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Input;
+
+namespace Mediag.ViewModels
+{
+    class SignLogInViewModel
+    {
+        public Models.Doctor Doctor { get; set; }
+
+        public ICommand RegisterDoctorCommand { get; private set; }
+        private void DisplayMessage()
+        {
+            System.Windows.MessageBox.Show($"{Doctor.LastName} {Doctor.FirstName} {Doctor.Birthdate.ToShortDateString()} {Doctor.City} {Doctor.Email} {Doctor.PhoneNumber} {Doctor.Username}");
+        }
+
+        public SignLogInViewModel()
+        {
+            Doctor = new Models.Doctor();
+            RegisterDoctorCommand = new RelayCommand(o => true, o => DisplayMessage());
+        }
+    }
+}
