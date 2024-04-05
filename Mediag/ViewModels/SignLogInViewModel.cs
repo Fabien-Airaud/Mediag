cd0d7d55ba3275de4d7dddbfec1e785bd6e75989
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-05 18:54:43 -0400

    Add property for confirm password

--- Mediag/ViewModels/SignLogInViewModel.cs
+++ Mediag/ViewModels/SignLogInViewModel.cs
@@ -1,16 +1,41 @@
-﻿using System.Windows.Input;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
 
 namespace Mediag.ViewModels
 {
-    class SignLogInViewModel
+    class SignLogInViewModel : INotifyPropertyChanged
     {
         public Models.Doctor Doctor { get; set; }
+
+        private string _registerConfirmPassword = "";
+        public string RegisterConfirmPassword
+        {
+            get { return _registerConfirmPassword; }
+            set
+            {
+                if (_registerConfirmPassword != value)
+                {
+                    _registerConfirmPassword = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
         public ICommand RegisterDoctorCommand { get; private set; }
         private void DisplayMessage()
         {
-            System.Windows.MessageBox.Show($"{Doctor.LastName} {Doctor.FirstName} {Doctor.Birthdate.ToShortDateString()} {Doctor.City} {Doctor.Email} {Doctor.PhoneNumber} {Doctor.Username}");
+            System.Windows.MessageBox.Show($"{Doctor.LastName} {Doctor.FirstName} {Doctor.Birthdate.ToShortDateString()} {Doctor.City} {Doctor.Email} {Doctor.PhoneNumber} {Doctor.Username} {Doctor.Password}|{RegisterConfirmPassword}");
         }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
 
         public SignLogInViewModel()
         {
