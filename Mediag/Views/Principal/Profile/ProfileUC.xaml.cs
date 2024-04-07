13f7a227e103e98b25cc3db43c78b28bb48e7987
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-07 17:19:48 -0400

    Create profil user control

---
+++ Mediag/Views/Principal/Profile/ProfileUC.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Mediag.Views.Principal.Profile
+{
+    /// <summary>
+    /// Interaction logic for ProfileUC.xaml
+    /// </summary>
+    public partial class ProfileUC : UserControl
+    {
+        public ProfileUC()
+        {
+            InitializeComponent();
+        }
+    }
+}
