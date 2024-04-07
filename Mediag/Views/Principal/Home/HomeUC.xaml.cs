4fb23fd308caf11df355e590ba58d38e807093cd
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-07 15:16:24 -0400

    Create Home User control

---
+++ Mediag/Views/Principal/Home/HomeUC.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Mediag.Views.Principal.Home
+{
+    /// <summary>
+    /// Interaction logic for HomeUC.xaml
+    /// </summary>
+    public partial class HomeUC : UserControl
+    {
+        public HomeUC()
+        {
+            InitializeComponent();
+        }
+    }
+}
