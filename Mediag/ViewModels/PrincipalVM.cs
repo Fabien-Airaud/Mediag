811260d58b9cf0af6c403cff5a92310b84a0283d
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-07 19:11:18 -0400

    Add current content for Principal

--- Mediag/ViewModels/PrincipalVM.cs
+++ Mediag/ViewModels/PrincipalVM.cs
@@ -1,13 +1,33 @@
-﻿namespace Mediag.ViewModels
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Controls;
+
+namespace Mediag.ViewModels
 {
-    public class PrincipalVM
+    public class PrincipalVM(Models.Doctor doctor) : INotifyPropertyChanged
     {
-        public Models.Doctor Doctor { get; set; }
+        public Models.Doctor Doctor { get; set; } = doctor;
 
-
-        public PrincipalVM(Models.Doctor doctor)
+        private UserControl _principalContent = new Views.Principal.Home.HomeUC();
+        public UserControl PrincipalContent
         {
-            Doctor = doctor;
+            get { return _principalContent; }
+            set
+            {
+                if (_principalContent != value)
+                {
+                    _principalContent = value;
+                }
+                OnPropertyChanged();
+            }
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
