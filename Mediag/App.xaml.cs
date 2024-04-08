fab5c0a1f8330fc365b2dbc010688fec191120ce
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-08 17:58:59 -0400

    Change culture date format

--- Mediag/App.xaml.cs
+++ Mediag/App.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using System.Globalization;
 using System.Windows;
 
 namespace Mediag
@@ -9,6 +8,15 @@ namespace Mediag
     /// </summary>
     public partial class App : Application
     {
-    }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
 
+            CultureInfo ci = new("en-US");
+            ci.DateTimeFormat.ShortDatePattern = "yyyy-MM-dd";
+            ci.DateTimeFormat.LongDatePattern = "yyyy-MM-dd HH:mm:ss";
+            Thread.CurrentThread.CurrentCulture = ci;
+            Thread.CurrentThread.CurrentUICulture = ci;
+        }
+    }
 }
