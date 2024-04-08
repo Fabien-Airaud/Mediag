4ac7ad6892d566151662ec62804a5781a7efb641
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-08 18:44:41 -0400

    Create patient view and view model

---
+++ Mediag/ViewModels/PatientVM.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace Mediag.ViewModels
+{
+    public class PatientVM : INotifyPropertyChanged
+    {
+        public Models.Patient Patient { get; set; }
+        public Models.Patient OldPatient { get; set; }
+
+        public ObservableCollection<Models.Hospital> Hospitals { get; set; }
+
+        private string _editVisibility;
+        public string EditVisibility
+        {
+            get { return _editVisibility; }
+            set
+            {
+                if (_editVisibility != value)
+                {
+                    _editVisibility = value;
+                    OnPropertyChanged();
+                    ViewVisibility = value == "Visible" ? "Hidden" : "Visible";
+                }
+            }
+        }
+
+        private string _viewVisibility;
+        public string ViewVisibility
+        {
+            get { return _viewVisibility; }
+            set
+            {
+                if (_viewVisibility != value)
+                {
+                    _viewVisibility = value;
+                    OnPropertyChanged();
+                    EditVisibility = value == "Visible" ? "Hidden" : "Visible";
+                }
+            }
+        }
+
+        public ICommand EditCommand { get; private set; }
+        private void ActiveEdit()
+        {
+            EditVisibility = "Visible";
+            if (!Patient.Equals(OldPatient)) Patient.CopyTo(OldPatient);
+            Patient.Hospital = Hospitals.First(h => h.Id == Patient.HospitalId);
+        }
+
+        public ICommand SaveCommand { get; private set; }
+        private void SaveProfile()
+        {
+            //ViewVisibility = "Visible";
+            //if (Patient.Equals(OldPatient)) return; // No changes
+
+            //Patient = Models.Patient.UpdatePatient(Patient);
+            //MessageBox.Show("Profile saved.");
+        }
+
+        public ICommand CancelCommand { get; private set; }
+        private void CancelEdit()
+        {
+            //ViewVisibility = "Visible";
+            //if (!OldPatient.Equals(Patient)) OldPatient.CopyTo(Patient);
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        public PatientVM(Models.Patient? patient = null, bool isEditMode = true)
+        {
+            Patient = new Models.Patient();
+            patient?.CopyTo(Patient);
+            OldPatient = new Models.Patient();
+            Hospitals = new ObservableCollection<Models.Hospital>(Models.Hospital.GetHospitals());
+            _editVisibility = isEditMode || patient is null ? "Visible" : "Hidden"; // Edit mode is default when patient is null
+            _viewVisibility = isEditMode || patient is null ? "Hidden" : "Visible"; // View mode is default when patient is not null
+            EditCommand = new RelayCommand(_ => true, _ => ActiveEdit());
+            SaveCommand = new RelayCommand(_ => Patient.IsValidRegister, _ => SaveProfile());
+            CancelCommand = new RelayCommand(_ => true, _ => CancelEdit());
+        }
+    }
+}
---
+++ Mediag/Views/Principal/Patients/Patient.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace Mediag.Views.Principal.Patients
+{
+    /// <summary>
+    /// Interaction logic for Patient.xaml
+    /// </summary>
+    public partial class Patient : Window
+    {
+        public Patient(Models.Patient? patient = null, bool isEditMode = true)
+        {
+            InitializeComponent();
+            DataContext = new ViewModels.PatientVM(patient, isEditMode);
+        }
+    }
+}
