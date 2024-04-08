7001997d710da7f54128cedd847105d6060d986c
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-08 16:19:20 -0400

    Create patient list view

---
+++ Mediag/Views/Principal/Patients/PatientsListUC.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace Mediag.Views.Principal.Patients
+{
+    /// <summary>
+    /// Interaction logic for PatientsListUC.xaml
+    /// </summary>
+    public partial class PatientsListUC : UserControl
+    {
+        public PatientsListUC(Models.Doctor doctor)
+        {
+            InitializeComponent();
+            DataContext = new ViewModels.PatientListVM(doctor);
+        }
+    }
+}
