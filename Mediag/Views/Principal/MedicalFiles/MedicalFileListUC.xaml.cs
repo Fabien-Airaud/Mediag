1962931f57a47a5129226f836677676d489a3b45
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-09 09:52:12 -0400

    Create medical file list view

---
+++ Mediag/Views/Principal/MedicalFiles/MedicalFileListUC.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace Mediag.Views.Principal.MedicalFiles
+{
+    /// <summary>
+    /// Interaction logic for MedicalFileListUC.xaml
+    /// </summary>
+    public partial class MedicalFileListUC : UserControl
+    {
+        public MedicalFileListUC()
+        {
+            InitializeComponent();
+            DataContext = new ViewModels.MedicalFileListVM();
+        }
+    }
+}
