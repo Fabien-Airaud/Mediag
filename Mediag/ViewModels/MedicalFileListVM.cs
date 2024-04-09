8bb6bf7fa1b55982c47399a95dd19b2acd7de3bd
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-09 09:51:48 -0400

    Create medical file list view model

---
+++ Mediag/ViewModels/MedicalFileListVM.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Mediag.ViewModels
+{
+    public class MedicalFileListVM
+    {
+        public ObservableCollection<Models.MedicalFile> MedicalFiles { get; set; }
+
+        public MedicalFileListVM()
+        {
+            MedicalFiles = new ObservableCollection<Models.MedicalFile>();
+        }
+    }
+}
