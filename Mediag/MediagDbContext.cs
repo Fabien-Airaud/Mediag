eb347816375e92c53b21c48a8091eed486d79a1c
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-09 14:06:36 -0400

    Add medical files

--- Mediag/MediagDbContext.cs
+++ Mediag/MediagDbContext.cs
@@ -5,9 +5,10 @@ namespace Mediag
 {
     class MediagDbContext : DbContext
     {
-        public DbSet<Models.Hospital> Hospitals { get; set; }
-        public DbSet<Models.Doctor> Doctors { get; set; }
-        public DbSet<Models.Patient> Patients { get; set; }
+        public DbSet<Hospital> Hospitals { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<MedicalFile> MedicalFiles { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
