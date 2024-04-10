f646302c2119175c525ca335e0bc801302f9e78b
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-09 23:10:48 -0400

    Complete IllnessTypes and can get all in list

--- Mediag/Models/IllnessTypes.cs
+++ Mediag/Models/IllnessTypes.cs
@@ -1,9 +1,69 @@
-﻿namespace Mediag.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.CompilerServices;
+
+namespace Mediag.Models
 {
-    public class IllnessTypes
+    [Index(nameof(Name), IsUnique = true)]
+    public class IllnessTypes : INotifyPropertyChanged
     {
         public long Id { get; set; }
 
-        public string Name { get; set; } = null!;
+        private string _name = "";
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private bool _isValid;
+        [NotMapped]
+        public bool IsValid
+        {
+            get { return _isValid; }
+            protected set
+            {
+                if (_isValid != value)
+                {
+                    _isValid = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        protected virtual bool CheckIsValid()
+        {
+            return !string.IsNullOrWhiteSpace(Name);
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            if (propertyName != nameof(IsValid))
+            {
+                IsValid = CheckIsValid();
+            }
+        }
+
+        public static ICollection<IllnessTypes> GetIllnessTypes()
+        {
+            MediagDbContext mediagDbContext = new();
+            return [.. mediagDbContext.IllnessTypes]; // Copy the collection
+        }
+
+        public override string? ToString()
+        {
+            return Name;
+        }
     }
 }
