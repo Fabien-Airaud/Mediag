be047dc1dd5676a9c8529d5ef342521522a9f46e
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-09 22:42:37 -0400

    Create interface for medical data

---
+++ Mediag/Models/IMedicalData.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace Mediag.Models
+{
+    public interface IMedicalData : INotifyPropertyChanged
+    {
+        public long Id { get; set; }
+
+        public long? MedicalFileId { get; set; }
+        public MedicalFile? MedicalFile { get; set; }
+
+        public bool IsValid { get; }
+
+        public string[] Labels();
+
+        public string[] Values();
+    }
+}
