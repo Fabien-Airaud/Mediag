c4a2bbac6a4a93e8cfaf8c247abfdb9fcdf98a73
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-20 12:32:08 -0400

    Create BreastCancerData User control

---
+++ Mediag/Views/Principal/MedicalFiles/BreastCancerDataUC.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Mediag.Views.Principal.MedicalFiles
+{
+    /// <summary>
+    /// Interaction logic for BreastCancerDataUC.xaml
+    /// </summary>
+    public partial class BreastCancerDataUC : UserControl
+    {
+        public BreastCancerDataUC()
+        {
+            InitializeComponent();
+        }
+    }
+}
