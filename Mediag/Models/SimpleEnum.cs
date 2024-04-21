9be68bbe8c5b16d23b4d0fa4cfdeaa1398acba8d
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-20 20:38:16 -0400

    Create SimpleEnum

---
+++ Mediag/Models/SimpleEnum.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mediag.Models
+{
+    [Index(nameof(Name), IsUnique = true)]
+    public abstract class SimpleEnum : INotifyPropertyChanged
+    {
+        public long Id { get; set; }
+
+        private string _name = "";
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private bool _isValid;
+        [NotMapped]
+        public bool IsValid
+        {
+            get { return _isValid; }
+            protected set
+            {
+                if (_isValid != value)
+                {
+                    _isValid = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        protected virtual bool CheckIsValid()
+        {
+            return !string.IsNullOrWhiteSpace(Name);
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            if (propertyName != nameof(IsValid))
+            {
+                IsValid = CheckIsValid();
+            }
+        }
+
+        public override string? ToString()
+        {
+            return Name;
+        }
+    }
+}
