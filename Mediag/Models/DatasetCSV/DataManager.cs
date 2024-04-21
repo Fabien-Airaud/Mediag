a393bceb52b330519759621af253deec45ad33ad
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-21 15:43:00 -0400

    Create dataManager

---
+++ Mediag/Models/DatasetCSV/DataManager.cs
@@ -0,0 +1,27 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System.Collections;
+using System.Globalization;
+using System.IO;
+
+namespace Mediag.Models.DatasetCSV
+{
+    public class DataManager<T, K> where T : IMedicalData where K : ClassMap<T>
+    {
+        public static List<T> GetCSVData(string filename)
+        {
+            if (!File.Exists(filename)) { return []; }
+
+            List<T> dataList = [];
+            using (var reader = new StreamReader(filename))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                csv.Context.RegisterClassMap<K>();
+                IEnumerable records = csv.GetRecords<T>();
+
+                foreach (T record in records) dataList.Add(record);
+            }
+            return dataList;
+        }
+    }
+}
