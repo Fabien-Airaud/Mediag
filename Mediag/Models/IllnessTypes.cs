5ca599e1e7c8462d39fc7109ea40f8c8c9267718
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-20 20:38:35 -0400

    Use SimpleEnum for enum IllnessTypes

--- Mediag/Models/IllnessTypes.cs
+++ Mediag/Models/IllnessTypes.cs
@@ -1,69 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Runtime.CompilerServices;
-
-namespace Mediag.Models
+﻿namespace Mediag.Models
 {
-    [Index(nameof(Name), IsUnique = true)]
-    public class IllnessTypes : INotifyPropertyChanged
+    public class IllnessTypes : SimpleEnum
     {
-        public long Id { get; set; }
-
-        private string _name = "";
-        public string Name
-        {
-            get { return _name; }
-            set
-            {
-                if (_name != value)
-                {
-                    _name = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        private bool _isValid;
-        [NotMapped]
-        public bool IsValid
-        {
-            get { return _isValid; }
-            protected set
-            {
-                if (_isValid != value)
-                {
-                    _isValid = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-        protected virtual bool CheckIsValid()
-        {
-            return !string.IsNullOrWhiteSpace(Name);
-        }
-
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-            if (propertyName != nameof(IsValid))
-            {
-                IsValid = CheckIsValid();
-            }
-        }
-
         public static ICollection<IllnessTypes> GetIllnessTypes()
         {
             MediagDbContext mediagDbContext = new();
             return [.. mediagDbContext.IllnessTypes]; // Copy the collection
-        }
-
-        public override string? ToString()
-        {
-            return Name;
         }
     }
 }
