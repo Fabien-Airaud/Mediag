c0b59fac04c8313b6e3981a29f0770c796d6ed65
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-21 12:01:11 -0400

    Create DecisionTree view model and add it to view

---
+++ Mediag/ViewModels/DecisionTreeVM.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Mediag.ViewModels
+{
+    public class DecisionTreeVM
+    {
+        public Models.IllnessTypes TargetIllness { get; set; }
+
+        public ObservableCollection<Models.IllnessTypes> IllnessTypes { get; set; }
+
+        private string _evaluateVisibility = "Hidden";
+        public string EvaluateVisibility
+        {
+            get { return _evaluateVisibility; }
+            set
+            {
+                if (_evaluateVisibility != value)
+                {
+                    _evaluateVisibility = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        public DecisionTreeVM()
+        {
+            IllnessTypes = new ObservableCollection<Models.IllnessTypes>(Models.IllnessTypes.GetIllnessTypes());
+            TargetIllness = IllnessTypes.First();
+        }
+    }
+}
