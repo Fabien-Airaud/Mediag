315a04999d9936d8ca12e494202ab2b64d77550c
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-20 21:40:17 -0400

    Create MajorVesselsTypes

---
+++ Mediag/Models/MajorVesselsTypes.cs
@@ -0,0 +1,11 @@
+﻿namespace Mediag.Models
+{
+    public class MajorVesselsTypes : SimpleEnum
+    {
+        public static ICollection<MajorVesselsTypes> GetMajorVesselsTypes()
+        {
+            MediagDbContext mediagDbContext = new();
+            return [.. mediagDbContext.MajorVesselsTypes]; // Copy the collection
+        }
+    }
+}
