b6779156a5dfae319ce66ff6a6bf400f053d2a00
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-20 21:29:55 -0400

    Create ChestPainTypes

---
+++ Mediag/Models/ChestPainTypes.cs
@@ -0,0 +1,11 @@
+﻿namespace Mediag.Models
+{
+    public class ChestPainTypes : SimpleEnum
+    {
+        public static ICollection<ChestPainTypes> GetChestPainTypes()
+        {
+            MediagDbContext mediagDbContext = new();
+            return [.. mediagDbContext.ChestPainTypes]; // Copy the collection
+        }
+    }
+}
