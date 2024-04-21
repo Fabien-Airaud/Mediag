ff61f61610390896458246e3d89b2039e01ab521
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-20 21:35:48 -0400

    Create ThalassemiaTypes

---
+++ Mediag/Models/ThalassemiaTypes.cs
@@ -0,0 +1,11 @@
+﻿namespace Mediag.Models
+{
+    public class ThalassemiaTypes : SimpleEnum
+    {
+        public static ICollection<ThalassemiaTypes> GetThalassemiaTypes()
+        {
+            MediagDbContext mediagDbContext = new();
+            return [.. mediagDbContext.ThalassemiaTypes]; // Copy the collection
+        }
+    }
+}
