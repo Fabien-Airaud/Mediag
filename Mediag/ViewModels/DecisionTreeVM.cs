c113652cfdfb0686abe4bbcc1fe4c554a15fe829
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-21 14:50:00 -0400

    Can train et test decision tree (false for now)

--- Mediag/ViewModels/DecisionTreeVM.cs
+++ Mediag/ViewModels/DecisionTreeVM.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections.ObjectModel;
+﻿using DiagnosticDecision;
+using Microsoft.Win32;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Input;
@@ -30,20 +31,57 @@ namespace Mediag.ViewModels
         public ICommand TrainCommand { get; private set; }
         private void Train()
         {
-            MessageBoxResult result = MessageBox.Show("Training...", "Training", MessageBoxButton.OK);
-            if (result.Equals(MessageBoxResult.OK))
+            // Configure open file dialog box
+            OpenFileDialog openFileDialog = new()
             {
-                EvaluateVisibility = "Visible";
+                FileName = "train", // Default file name
+                DefaultExt = ".csv", // Default file extension
+                Filter = "CSV Files (*.csv)|*.csv" // Filter files by extension
+            };
+
+            // Process open file dialog box
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filename = openFileDialog.FileName;
+                if (filename == null || !System.IO.File.Exists(filename))
+                {
+                    MessageBox.Show("File not found", "File error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                // Train the decision tree
+                TrainDecisionTree(filename);
+
+                MessageBoxResult result = MessageBox.Show("Training successful", "Training", MessageBoxButton.OK);
+                if (result == MessageBoxResult.OK) EvaluateVisibility = "Visible";
             }
         }
 
         public ICommand EvaluateCommand { get; private set; }
         private void Evaluate()
         {
-            MessageBoxResult result = MessageBox.Show("Evaluating...", "Evaluating", MessageBoxButton.OK);
-            if (result.Equals(MessageBoxResult.OK))
+            // Configure open file dialog box
+            OpenFileDialog openFileDialog = new()
             {
-                EvaluateVisibility = "Hidden";
+                FileName = "test", // Default file name
+                DefaultExt = ".csv", // Default file extension
+                Filter = "CSV Files (*.csv)|*.csv" // Filter files by extension
+            };
+
+            // Process open file dialog box
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filename = openFileDialog.FileName;
+                if (filename == null || !System.IO.File.Exists(filename))
+                {
+                    MessageBox.Show("File not found", "File error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                // Evaluate the decision tree
+                EvaluateDecisionTree(filename);
+
+                MessageBox.Show("Evaluation successful", "Evaluation", MessageBoxButton.OK);
             }
         }
 
@@ -63,6 +101,42 @@ namespace Mediag.ViewModels
 
             TrainCommand = new RelayCommand(_ => true, _ => Train());
             EvaluateCommand = new RelayCommand(_ => true, _ => Evaluate());
+        }
+
+        private void TrainDecisionTree(string filename)
+        {
+            // Get the decision tree
+            IDecisionTree tree;
+            if (Models.Hospital.DecisionTrees.TryGetValue(TargetIllness, out IDecisionTree? value))
+            {
+                tree = value;
+            }
+            else
+            {
+                tree = new DecisionTree(TargetIllness.Name);
+                Models.Hospital.DecisionTrees.Add(TargetIllness, tree);
+            }
+
+            // Train the decision tree
+            //tree.BuildTree([], []); // TODO: Implement this
+        }
+
+        private void EvaluateDecisionTree(string filename)
+        {
+            // Get the decision tree
+            IDecisionTree tree;
+            if (Models.Hospital.DecisionTrees.TryGetValue(TargetIllness, out IDecisionTree? value))
+            {
+                tree = value;
+            }
+            else
+            {
+                MessageBox.Show("No decision tree for " + TargetIllness, "Error Decision tree", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            // Evaluate the decision tree
+            //tree.Evaluate([], out _, out _, out _); // TODO: Implement this
         }
     }
 }
