8cc30cdb4e8c9f7caa829e594888b45cbe8e957f
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-20 22:49:36 -0400

    Create HeartDiseaseData user control

---
+++ Mediag/Views/Principal/MedicalFiles/HeartDiseaseDataUC.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Mediag.Views.Principal.MedicalFiles
+{
+    /// <summary>
+    /// Interaction logic for HeartDiseaseDataUC.xaml
+    /// </summary>
+    public partial class HeartDiseaseDataUC : UserControl
+    {
+        public HeartDiseaseDataUC()
+        {
+            InitializeComponent();
+        }
+    }
+}
