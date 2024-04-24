291a092ca0a1666f5bc37a1e7b28bf5c2e3f22d4
Fabien-Airaud <airaudfabien@gmail.com>
2024-04-23 21:11:25 -0400

    Add isValidCSV

--- Mediag/Models/DatasetCSV/DataManager.cs
+++ Mediag/Models/DatasetCSV/DataManager.cs
@@ -10,8 +10,6 @@ namespace Mediag.Models
     {
         public static List<T> GetCSVData(string filename)
         {
-            if (!File.Exists(filename)) { return []; }
-
             List<T> dataList = [];
             using (var reader = new StreamReader(filename))
             using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
@@ -22,6 +20,24 @@ namespace Mediag.Models
                 foreach (T record in records) dataList.Add(record);
             }
             return dataList;
+        }
+
+        public static bool IsValidCSV(string filename)
+        {
+            if (!File.Exists(filename)) return false;
+
+            using (var reader = new StreamReader(filename))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                csv.Read();
+                csv.ReadHeader();
+                csv.Context.RegisterClassMap<K>();
+
+                csv.Read();
+                try { T record = csv.GetRecord<T>(); } // Check if data in the CSV file is valid
+                catch (CsvHelperException) { return false; }
+            }
+            return true;
         }
     }
 }
